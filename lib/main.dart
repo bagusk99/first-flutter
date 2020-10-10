331df7c19a5409ca1b3185ae3f753d9c31abe2a2
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-10-11 01:36:54 +0700

    create first list

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,56 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcome to Flutter'),
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            //1
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //2
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500]
+                  )
+                )
+              ],
+            )
           ),
-          body: Center(child: Text('Hello World'))),
+          //3
+          Icon(
+            Icons.star,
+            color: Colors.red
+          ),
+          Text('41')
+        ],
+      )
+    );
+
+    return MaterialApp(
+      title: 'Flutter Apps',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Selamat Datang'),
+        ),
+        body: Column(
+          children: [
+            titleSection
+          ]
+        )
+      ),
     );
   }
 }
